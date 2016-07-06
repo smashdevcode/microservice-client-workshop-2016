bba55f5850f504ead6dcd74c16ef114f5b1d5d4b
Beau Palmquist <beau_g_palmquist@homedepot.com>
2016-07-06 15:49:26 -0700

    Added Vanilla starter project

---
+++ projects/Microservice.Clients/Vanilla.Starter/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Vanilla.Starter
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
